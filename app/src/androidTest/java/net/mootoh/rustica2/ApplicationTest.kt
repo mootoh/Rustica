638a7a911055fd8589e274e8150db9ed007dc2d1
Motohiro Takayama <mootoh@gmail.com>
2020-12-09 22:18:47 +0900

    Converted to Kotlin

---
+++ app/src/androidTest/java/net/mootoh/rustica2/ApplicationTest.kt
@@ -0,0 +1,8 @@
+package net.mootoh.rustica2
+
+import android.app.Application
+import android.test.ApplicationTestCase
+/**
+ * [Testing Fundamentals](http://d.android.com/tools/testing/testing_android.html)
+ */
+class ApplicationTest : ApplicationTestCase<Application?>(Application::class.java)
